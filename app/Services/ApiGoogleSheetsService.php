9537a547f107a11ba3487c6d7f7dd26f3ca63335
DouradoCtrl <gdouradoestudante@gmail.com>
2025-12-28 13:18:55 -0300

    add: created service for operation the google sheets

---
+++ app/Services/ApiGoogleSheetsService.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Services;
+
+use Google\Client;
+use Google\Service\Sheets;
+
+class ApiGoogleSheetsService
+{
+    protected $client;
+    protected $service;
+
+    public function __construct()
+    {
+        $this->client =  new Client();
+
+        $this->client->setAuthConfig(storage_path('app/private/google-sheets/jp-duarte-dev-google-sheets.json'));
+        $this->client->addScope(Sheets::SPREADSHEETS);
+
+        $this->service = new Sheets($this->client);
+    }
+}
